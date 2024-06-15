c63a840bd590172b89147a40217f70400cf2cda4
Andrey Masloboev <amasloboev@topcon.com>
2024-06-15 15:55:07 +0300

    done build graph for develop

--- fl.py
+++ fl.py
@@ -4,6 +4,7 @@ import os
 
 command = ['git', 'clone', '--depth', '1', 'http://github.com/brendangregg/FlameGraph']
 subprocess.run(command)
+project_flgr = os.path.abspath('FlameGraph')
 
 project_dir = os.getcwd()
 print(f"Directory of project: {project_dir}")
@@ -21,6 +22,37 @@ flags = "-g -fno-omit-frame-pointer"
 #build develop
 subprocess.run(['cmake', '-S', project_dir, '-B', build_dev_dir, '-DCMAKE_C_FLAGS=' + flags, '-DCMAKE_CXX_FLAGS=' + flags])
 subprocess.run(['cmake', '--build', build_dev_dir])
+
+#
+tests_dir = build_dev_dir + '/tests'
+tests_execut = []
+
+for filename in os.listdir(tests_dir):
+    filepath = os.path.join(tests_dir, filename)
+    if filename.startswith('test') and os.access(filepath, os.X_OK):
+        tests_execut.append(filepath)
+
+print(tests_execut)
+
+for test in tests_execut:
+    profile_path = os.path.join(tests_dir, 'profile', os.path.basename(test))
+    record_data = os.path.join(profile_path, 'perf.data')
+    os.makedirs(profile_path, exist_ok=True)
+    record = 'perf record -F 999 -a -g -o ' + record_data + ' -- ' + test
+    print(record)
+    subprocess.run(record, shell=True)
+
+    #perf script | ./stackcollapse-perf.pl > out.perf
+    script_data = os.path.join(profile_path, 'out.perf')
+    script = 'perf script -i ' + record_data + ' | ' + os.path.join(project_flgr, 'stackcollapse-perf.pl') +' > '+ script_data
+    subprocess.run(script, shell=True)
+
+    #./flamegraph.pl out.perf > flamegraph.svg
+    svg_data = os.path.join(profile_path, 'flamegraph.svg')
+    svg = os.path.join(project_flgr, 'flamegraph.pl') + ' ' + script_data + ' > ' + svg_data
+    subprocess.run(svg, shell=True)
+
+
 
 
 
