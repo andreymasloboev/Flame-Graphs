a8be11a308f07ad049c32b9eb8d38d80bd1e7e16
Andrey Masloboev <amasloboev@topcon.com>
2024-06-15 16:41:48 +0300

    done

--- fl.py
+++ fl.py
@@ -39,8 +39,8 @@ def buildGraph(build_dir):
         print(record)
         subprocess.run(record, shell=True)
 
-        #perf script | ./stackcollapse-perf.pl > out.perf
-        script_data = os.path.join(profile_path, 'out.perf')
+        #perf script | ./stackcollapse-perf.pl > out.folded
+        script_data = os.path.join(profile_path, 'out.folded')
         script = 'perf script -i ' + record_data + ' | ' + os.path.join(project_flgr, 'stackcollapse-perf.pl') +' > '+ script_data
         subprocess.run(script, shell=True)
 
@@ -48,6 +48,29 @@ def buildGraph(build_dir):
         svg_data = os.path.join(profile_path, 'flamegraph.svg')
         svg = os.path.join(project_flgr, 'flamegraph.pl') + ' ' + script_data + ' > ' + svg_data
         subprocess.run(svg, shell=True)
+
+def buildDiffGraph(build_dir, build_br_dir):
+
+    tests_dir = build_dir + '/tests'
+    tests_execut = []
+
+    for filename in os.listdir(tests_dir):
+        filepath = os.path.join(tests_dir, filename)
+        if filename.startswith('test') and os.access(filepath, os.X_OK):
+            tests_execut.append(filepath)
+
+    print(tests_execut)
+
+    for test in tests_execut:
+        profile_diff = os.path.join(tests_dir, 'profile_diff', os.path.basename(test))
+        os.makedirs(profile_diff, exist_ok=True)
+        profile_diff=os.path.join(profile_diff, 'diff2.svg')
+        develop_data=os.path.join(build_dir,'tests','profile', os.path.basename(test), 'out.folded')
+        branch_data=os.path.join(build_br_dir,'tests','profile', os.path.basename(test), 'out.folded')
+        # ./difffolded.pl out.folded1 out.folded2 | ./flamegraph.pl > diff2.svg
+        diff_cmd = os.path.join(project_flgr, 'difffolded.pl') + ' ' + develop_data + ' ' + branch_data + ' | ' + os.path.join(project_flgr, 'flamegraph.pl') + ' > ' + profile_diff
+        subprocess.run(diff_cmd, shell=True)
+        print(diff_cmd)
 
 build_dev_dir = os.getcwd() +'/build_develop'
 print(f"Build directory for develop: {build_dev_dir}")
@@ -64,6 +87,9 @@ switchGitBranch('exp/test')
 
 #build branch
 buildGraph(build_br_dir)
+
+#build diff, we must copmare only test from develop for ignore new tests
+buildDiffGraph(build_dev_dir, build_br_dir)
 
 
 
